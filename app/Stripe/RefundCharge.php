287b2b245356537810b63fd17008adeb597a5fb4
João Melo <joao@cloudonline.com.br>
2021-04-24 00:56:59 -0300

    first changes.

--- app/Stripe/RefundCharge.php
+++ app/Stripe/RefundCharge.php
@@ -10,7 +10,7 @@ class RefundCharge
      * @param string $token string that identifies the target account.
      * @param int $amount in cents
      */
-    public function refund(string $token, int $amount)
+    public static function refund(string $token, int $amount)
     {
         sleep(10);
 
